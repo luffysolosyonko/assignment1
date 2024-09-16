c44aff8f3e590c810d516808f382e298e34584cf
luffysolosyonko <116454436+luffysolosyonko@users.noreply.github.com>
2024-09-15 23:43:41 -0500

    submitted phonebook.rb

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,58 @@
 class PhoneBook
     def initialize
+        @hash={}
+        @hashfornumbertoname={}
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        
+        return false unless  !@hash.key?(name)
+
+            return false unless /\A\d{3}-\d{3}-\d{4}\z/.match?(number)
+
+                if @hash.has_value?(@hash[name]) and @hash[number]
+                    return false
+                else
+                    @hash[name]=number
+                    @hash[number]=is_listed
+                    @hashfornumbertoname[number]=name
+                    
+            
+                    
+                    return true
+                end
+                
+            
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        
+        if @hash.key?(name) and @hash[@hash[name]]
+            return @hash[name]
+        else
+            return nil
+        end
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        if @hash[number] #if it's listed==true
+            
+            return @hashfornumbertoname[number]
+        else
+            return nil
+        end
+
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        areacodecomp=areacode.to_s[0,3]
+        compar=@hash.select do |name, number|
+            tempareacode=number.to_s[0,3]
+            tempareacode==areacode
+        end.keys
+        compar
+            
+        end
     end
-end
+
+
